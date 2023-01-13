6ac93db67110e441a23639e3f118380931d8609a
FloreDenis2001 <floredenis907@yahoo.com>
2023-01-13 23:44:20 +0200

    update_show_cars

--- src/main/java/ro/mycode/autovitapi/AutovitApiApplication.java
+++ src/main/java/ro/mycode/autovitapi/AutovitApiApplication.java
@@ -1,13 +1,30 @@
 package ro.mycode.autovitapi;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import ro.mycode.autovitapi.service.MasinaService;
 
 @SpringBootApplication
 public class AutovitApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AutovitApiApplication.class, args);
+	}
+
+
+
+	@Bean
+	CommandLineRunner commandLineRunner(MasinaService masinaService){
+		return  args->{
+
+
+
+			masinaService.getAllCars().forEach(System.out::println);
+
+		};
+
 	}
 
 }
